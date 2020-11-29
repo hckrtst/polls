9f982902f40909f32442f45fadc812857ad7773b
SK <hellosanketk@gmail.com>
2020-11-29 10:22:02 -0800

    add header with logo

---
+++ north-poll/src/components/Header.svelte
@@ -0,0 +1,23 @@
+<style>
+  header {
+    background: #f7f7f7;
+    padding: 20px;
+  }
+
+  h1 {
+    text-align: center;
+  }
+
+  img {
+    max-width: 200px;
+  }
+</style>
+
+<header>
+  <h1>
+    <div>
+      <img src="img/element5-digital-T9CXBZLUvic-unsplash.jpg" alt="logo" />
+    </div>
+    <div>The North Poll</div>
+  </h1>
+</header>
