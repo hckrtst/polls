d23c876afe7a6511448f2d8bff240c1d536ef838
SK <hellosanketk@gmail.com>
2020-12-05 12:46:10 -0800

    add form fields

--- north-poll/src/components/AddPollForm.svelte
+++ north-poll/src/components/AddPollForm.svelte
@@ -1,3 +1,20 @@
 <script></script>
-<form>placeholder</form>
-<style></style>
+<form>
+  <div class="form-field">
+    <label for="question">Question</label>
+    <input type="text" id="question">
+  </div>
+  <div class="form-field">
+    <label for="answer-a">Option</label>
+    <input type="text" id="answer-a">
+  </div>
+  <div class="form-field">
+    <label for="answer-b">Option</label>
+    <input type="text" id="answer-b">
+  </div>
+</form>
+<style>
+  .form-field{
+    text-align: center;
+  }
+</style>
