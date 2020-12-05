709e615d72026d1b389fb8727f21a311cdd11da3
SK <hellosanketk@gmail.com>
2020-12-05 13:42:53 -0800

    styling and add submission handler

--- north-poll/src/components/AddPollForm.svelte
+++ north-poll/src/components/AddPollForm.svelte
@@ -1,20 +1,46 @@
-<script></script>
-<form>
+<script>
+  let fields = {
+    ques: '',
+    ans_a: '',
+    ans_b: ''
+  };
+  function handleAdd() {
+    console.log(fields);
+  }
+</script>
+<form on:submit|preventDefault={handleAdd}>
   <div class="form-field">
-    <label for="question">Question</label>
-    <input type="text" id="question">
+    <label for="question">Question:</label>
+    <input type="text" id="question" bind:value={fields.ques}>
   </div>
   <div class="form-field">
-    <label for="answer-a">Option</label>
+    <label for="answer-a">Answer A:</label>
     <input type="text" id="answer-a">
   </div>
   <div class="form-field">
-    <label for="answer-b">Option</label>
+    <label for="answer-b">Answer B:</label>
     <input type="text" id="answer-b">
   </div>
+  <button>Add</button>
 </form>
+
 <style>
-  .form-field{
+  form{
+    width: 400px;
+    margin: 0 auto;
     text-align: center;
   }
+  .form-field{
+    margin: 18px auto;
+
+  }
+  input{
+    width: 100%;
+    border-radius:  6px;
+  }
+  label{
+    margin: 10px auto;
+    text-align: left;
+  }
+  
 </style>
